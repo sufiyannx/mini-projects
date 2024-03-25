d0367ce7532f88d15e88eb3561d5f144e3880df7
Sufiyan Ali <155474207+sufiyannx@users.noreply.github.com>
2024-03-25 21:10:56 +0530

    Add files via upload

---
+++ lecture 1/script.js
@@ -0,0 +1,116 @@
+// fullname = "Sufiyan Ali";
+// age = 21;
+// price = 99.99;
+// x = null;
+// y = undefined;
+// console.log(x);
+// console.log(y);
+// // console.log(fullname);
+// console.log(age);
+// console.log(price);
+
+// isfollow = false;
+// console.log(isfollow);
+// isfollow = true;
+// console.log(isfollow);
+
+// fullname = 25;
+// console.log(fullname);
+
+// Console = "Ali";
+// console.log(Console);
+
+// let sufi = 200;
+// console.log(sufi);
+
+// sufi = 100;
+// console.log(sufi);
+
+// let fullName = "sufiyan";
+// console.log(fullName);
+
+// const a = 1123;
+// console.log(a);
+
+// console.log(x);
+// var a = "ali";
+// let a = "ali";
+// const a = "ali"
+// console.log(a);
+
+// data type
+//premitive datatype, 7
+//number, string, boolean, undefined, null, bigint, symbol
+
+// let age = 21;
+// console.log(age);
+// console.log(typeof age);
+
+// let fullName = "sufiyan";
+// console.log(fullName);
+// console.log(typeof fullName);
+
+// isFollow = true;
+// console.log(isFollow);
+// console.log(typeof isFollow);
+
+// let x ;
+// console.log(x);
+// console.log(typeof x);
+
+// let y = null;
+// console.log(y);
+// console.log(typeof y);
+
+// let z = BigInt("123");
+// console.log(z);
+// console.log(typeof z);
+
+// let w = Symbol("hello");
+// console.log(w);
+// console.log(typeof w);
+
+//non-premitive--object(array and function)
+//obj is collection of values
+//info of a student id obj like age, marks, address, result
+// like a key : value
+
+// const student = {
+//     fullName : "sufiyan ali",
+//     age : 21,
+//     address : "jaitpur",
+//     cgpa :8.3,
+//     isPass :true,
+// };
+
+// student["age"] = student["age"] + 1;
+// student["fullName"] = "Sufiyan Ali"
+// console.log(student);
+// console.log(student["age"]);
+// console.log(student.address);
+
+//we cannot update or change const but we can change key value of object present in const
+
+//practice Q1//
+
+// const product = {
+//     fullName: "Parker Jotter Standard CT Ball Pen(Black)",
+//     rating: 4,
+//     Price: 270,
+//     offer: 5,
+// };
+
+
+const profile = {
+    userName : "sufiyanali._",
+    fullName : "sufiyan ali",
+    isFollow : true,
+    Posts : 195,
+    followers : 569000,
+    following : 4,
+    title : "entrepreneur",
+    bio : "apna college | ex-microsoft"
+};
+console.log(profile);
+console.log(typeof profile);
+console.log(typeof profile["userName"]); 
