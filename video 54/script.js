bdbae058e364e5dd9052a29c8591236c2aff9c71
Sufiyan Ali <155474207+sufiyannx@users.noreply.github.com>
2024-04-08 23:44:35 +0530

    Add files via upload

---
+++ video 54/script.js
@@ -0,0 +1,22 @@
+alert("hello world");
+
+console.log("hey i am sufiyan");
+console.log("hey i am sufiyan2");
+console.log("hey i am sufiyan3");
+
+var a = prompt("enter your number");
+var isTrue = confirm("you sure?")
+
+if(isTrue){
+    console.log("yes")
+}
+
+else{
+    console.log("no")
+}
+
+console.log("your number is " +a);
+
+document.title = "hey im good"
+
+document.body.style.backgroundColor = "red"
